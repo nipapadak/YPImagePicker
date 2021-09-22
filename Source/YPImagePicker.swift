31a6541f7ee8f09f45860b070d4f66cefcedfeb1
Nikolaos Papadakis <nipapadak@gmail.com>
2021-09-22 12:41:33 -0400

    Over full screen to prevent iOS 14.0 crash

--- Source/YPImagePicker.swift
+++ Source/YPImagePicker.swift
@@ -51,7 +51,7 @@ open class YPImagePicker: UINavigationController {
         YPImagePickerConfiguration.shared = configuration
         picker = YPPickerVC()
         super.init(nibName: nil, bundle: nil)
-        modalPresentationStyle = .fullScreen // Force .fullScreen as iOS 13 now shows modals as cards by default.
+        modalPresentationStyle = .overFullScreen // Force .fullScreen as iOS 13 now shows modals as cards by default.
         picker.imagePickerDelegate = self
         navigationBar.tintColor = .ypLabel
     }
